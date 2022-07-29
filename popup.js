fbdff6318e9198c16cf27eb5cb442235fb51956e
degisew <jegisew21@gmail.com>
2022-07-29 03:39:43 +0300

    popup window javascript file added

---
+++ popup.js
@@ -0,0 +1,229 @@
+let projects = [
+  {
+    title: "Tonic",
+    frame: {
+      canopy: "Canopy",
+      disc: '<img src="./images/Counter.png" alt="counter year" />',
+      backDev: "backend dev",
+      year: 2015,
+    },
+    Image: "./images/card-1.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    technoloies: {
+      html: "html",
+      css: "css",
+      javascript: "javaScript",
+    },
+    githubLink: "https://github.com/degisew/degisew.github.io",
+    demoLink: "https://degisew.github.io",
+  },
+  {
+    title: "Multipost Stories",
+    frame: {
+      canopy: "Canopy",
+      disc: '<img src="./images/Counter.png" alt="counter year" />',
+      backDev: "backend dev",
+      year: 2015,
+    },
+    Image: "./images/card-2.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    technoloies: {
+      html: "html",
+      css: "css",
+      javascript: "javaScript",
+    },
+    githubLink: "https://github.com/degisew/degisew.github.io",
+    demoLink: "https://degisew.github.io",
+  },
+  {
+    title: "Tonic",
+    frame: {
+      canopy: "Canopy",
+      disc: '<img src="./images/Counter.png" alt="counter year" />',
+      backDev: "backend dev",
+      year: 2015,
+    },
+    Image: "./images/card-3.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    technoloies: {
+      html: "html",
+      css: "css",
+      javascript: "javaScript",
+    },
+    githubLink: "https://github.com/degisew/degisew.github.io",
+    demoLink: "https://degisew.github.io",
+  },
+  {
+    title: "Multipost stories",
+    frame: {
+      canopy: "Canopy",
+      disc: '<img src="./images/Counter.png" alt="counter year" />',
+      backDev: "backend dev",
+      year: 2015,
+    },
+    Image: "./images/card-4.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    technoloies: {
+      html: "html",
+      css: "css",
+      javascript: "javaScript",
+    },
+    githubLink: "https://github.com/degisew/degisew.github.io",
+    demoLink: "https://degisew.github.io",
+  },
+  {
+    title: "Tonic",
+    frame: {
+      canopy: "Canopy",
+      disc: '<img src="./images/Counter.png" alt="counter year" />',
+      backDev: "backend dev",
+      year: 2015,
+    },
+    Image: "./images/desktop-card1.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    technoloies: {
+      html: "html",
+      css: "css",
+      javascript: "javaScript",
+    },
+    githubLink: "https://github.com/degisew/degisew.github.io",
+    demoLink: "https://degisew.github.io",
+  },
+  {
+    title: "Multi post Stories",
+    frame: {
+      canopy: "Facebook",
+      disc: '<img src="./images/Counter.png" alt="counter year" />',
+      backDev: "full-stack dev",
+      year: 2015,
+    },
+    Image: "./images/desktop-card2.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    technoloies: {
+      html: "html",
+      ruby: "Ruby on Rils",
+      css: "css",
+      javascript: "javaScript",
+    },
+    githubLink: "https://github.com/degisew/degisew.github.io",
+    demoLink: "https://degisew.github.io",
+  },
+  {
+    title: "Facebook 360",
+    frame: {
+      canopy: "Facebook",
+      disc: '<img src="./images/Counter.png" alt="counter year" />',
+      backDev: "full-stack dev",
+      year: 2015,
+    },
+    Image: "./images/desktop-card3.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    technoloies: {
+      html: "html",
+      ruby: "Ruby on Rils",
+      css: "css",
+      javascript: "javaScript",
+    },
+    githubLink: "https://github.com/degisew/degisew.github.io",
+    demoLink: "https://degisew.github.io",
+  },
+  {
+    title: "Uber Navigation",
+    frame: {
+      canopy: "Uber",
+      disc: '<img src="./images/Counter.png" alt="counter year" />',
+      backDev: "Lead developer",
+      year: 2018,
+    },
+    Image: "./images/desktop-card4.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    technoloies: {
+      html: "html",
+      ruby: "Ruby on Rils",
+      css: "css",
+      javascript: "javaScript",
+    },
+    githubLink: "https://github.com/degisew/degisew.github.io",
+    demoLink: "https://degisew.github.io",
+  },
+];
+
+const btn1 = document.getElementById("0");
+const submitBtn = document.querySelectorAll(".btn-submit");
+const closePopupBtn = document.getElementById("popup-menu-close-btn");
+var popupWindow = document.getElementById("popup-detail-window");
+submitBtn.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    const targetId = e.target.id;
+    const project = projects[targetId];
+    popupWindow.innerHTML =
+      popupWindow.innerHTML +
+      `
+      <div>
+      <h2 class="tonic">
+      ${project.title}
+      </h2>
+        <div class="tonic-frame">
+         <p class="canopy">
+        ${project.frame.canopy}
+        </p>
+        ${project.frame.disc}
+        <p class="back-end-dev">
+        ${project.frame.backDev}
+        </p>
+        ${project.frame.disc}
+        <p class="year">
+        ${project.frame.year}
+        </p>
+        </div>
+       <div class="popup-block-1-and-2-div">
+        <div class="popup-image">
+        <image src="${project.Image}"></image>
+        </div>
+       <div class="popup-block2-div">
+        <div class="popup-descrptn">
+        </p>
+        ${project.description}
+        <p>
+        </div>
+        <div class="popup-tech-and-see-btns-div">
+        <div class="ul-div">
+          <ul class="sub-block-3">
+            <li class="html">${project.technoloies.html}</li>
+            <li class="css">${project.technoloies.css}</li>
+            <li class="javascript">${project.technoloies.javascript}</li>
+          </ul>
+        </div>
+        <div class="popup-btn">
+        <button type="submit" value="See live" class= "popup-see-btn">
+        <div class="inside-popup-btn-div">
+
+        <a href="${project.demoLink}" class="see-anchor"><h3>See live</h3></a>
+        <img src="./images/seelive.png"/></div>
+        </button>
+        <button type="submit" class= "popup-see-btn">
+        <div class="inside-popup-btn-div">
+        <a href="${project.githubLink}" class="see-anchor"><h3>See Source</h3></a>
+        <img src="./images/popupgihubicon.png"/></div>
+        </button>
+        </div>
+      </div>
+       </div>
+       </div>
+        </div> `;
+    popupWindow.classList.toggle("visible");
+    document.body.style.overflow = 'hidden';
+ });
+});
+closePopupBtn.addEventListener("click", (e)=> {
+ console.log('text');
+// e.preventDefault();
+});
