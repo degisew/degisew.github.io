a051624adb6f80ed8c900d31b71e2de9336b29f1
degisew <jegisew21@gmail.com>
2022-07-29 23:04:27 +0300

    localStorage added

---
+++ localStorage.js
@@ -0,0 +1,17 @@
+
+const userName = document.getElementById('client-name');
+const userEmail = document.getElementById('client-email');
+const userMessage = document.getElementById('client-message');
+
+let userInput = {
+    un: userName.value,
+    email: userEmail.value,
+    message: userMessage.value,
+};
+
+localStorage.setItem('userData', JSON.stringify(userInput));
+const test = localStorage.getItem('userData');
+const parsedData = JSON.parse(test);
+userName.value = parsedData.un;
+userEmail.value = parsedData.email;
+userMessage.value = parsedData.message;
