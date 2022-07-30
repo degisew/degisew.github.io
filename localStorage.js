51058ec0492a6ab239744d9984689137eb84d8a1
degisew <jegisew21@gmail.com>
2022-07-30 14:09:33 +0300

    condititon check added to localstorage.js

--- localStorage.js
+++ localStorage.js
@@ -1,17 +1,23 @@
 const userName = document.getElementById('client-name');
 const userEmail = document.getElementById('client-email');
 const userMessage = document.getElementById('client-message');
-
+const form = document.getElementsByClassName('form');
 const userInput = {
   un: userName.value,
   email: userEmail.value,
   message: userMessage.value,
 };
 
+form.addEventListener('input', (event) => {
+  event.preventDefault();
+  localStorage.setItem('userData', JSON.stringify(userInput));
+  return true;
+});
 
-localStorage.setItem('userData', JSON.stringify(userInput));
 const test = localStorage.getItem('userData');
 const parsedData = JSON.parse(test);
-userName.value = parsedData.un;
-userEmail.value = parsedData.email;
-userMessage.value = parsedData.message;
+if (parsedData !== null) { // to check wheather the local storage is empty or not.
+  userName.value = parsedData.un;
+  userEmail.value = parsedData.email;
+  userMessage.value = parsedData.message;
+}
