67911b0f3621570e515962a8f8c4606c9848efe3
degisew <jegisew21@gmail.com>
2022-07-30 19:08:51 +0300

    approprate event added to the form inputs

--- localStorage.js
+++ localStorage.js
@@ -1,23 +1,37 @@
 const userName = document.getElementById('client-name');
 const userEmail = document.getElementById('client-email');
 const userMessage = document.getElementById('client-message');
-const form = document.getElementsByTagName('form')['0'];
+const form = document.getElementById('form-tag');
+
 const userInput = {
-  un: userName.value,
-  email: userEmail.value,
-  message: userMessage.value,
+  un: '',
+  email: '',
+  message: '',
 };
 
-form.addEventListener('input', (event) => {
-  event.preventDefault();
-  localStorage.setItem('userData', JSON.stringify(userInput));
-  return true;
-});
-
-const test = localStorage.getItem('userData');
-const parsedData = JSON.parse(test);
-if (parsedData !== null) { // to check wheather the local storage is empty or not.
+// to check wheather the local storage has data or not.
+if (localStorage.getItem('userData')) {
+  const test = localStorage.getItem('userData');
+  const parsedData = JSON.parse(test);
   userName.value = parsedData.un;
   userEmail.value = parsedData.email;
   userMessage.value = parsedData.message;
 }
+
+userName.addEventListener('input', (event) => {
+  event.preventDefault();
+  userInput.un = userName.value;
+});
+userEmail.addEventListener('input', (event) => {
+  event.preventDefault();
+  userInput.email = userEmail.value;
+});
+userMessage.addEventListener('input', (event) => {
+  event.preventDefault();
+  userInput.message = userMessage.value;
+});
+
+form.addEventListener('input', (event) => {
+  event.preventDefault();
+  localStorage.setItem('userData', JSON.stringify(userInput));
+});
